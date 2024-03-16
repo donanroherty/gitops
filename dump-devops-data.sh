9f169570831b89a6a1fb8172c0503e971e73c48d
Ronan Doherty <ronan.doherty@sse.com>
2024-03-16 17:30:57 +0000

    add existing cluster code

---
+++ dump-devops-data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Define an array of folder names
+folders=(".argocd" "kubernetes")
+
+# Function to process files recursively
+process_files() {
+    local folder="$1"
+    local output_section="$2"
+
+    echo "$output_section:" >> output.txt
+    echo "---" >> output.txt
+
+    # Recursively find all YAML files within the folder
+    while IFS= read -r -d '' file; do
+        echo "File: $file" >> output.txt
+        echo "---" >> output.txt
+        cat "$file" >> output.txt
+        echo >> output.txt
+    done < <(find "$folder" -type f -name '*.yaml' -print0)
+
+    echo >> output.txt
+}
+
+# Process files for each folder
+for folder in "${folders[@]}"; do
+    case "$folder" in
+        ".argocd")
+            process_files "$folder" "ArgoCD YAML files"
+            ;;
+        "kubernetes")
+            process_files "$folder" "Kubernetes YAML files"
+            ;;
+    esac
+done
